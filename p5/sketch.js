1e1f6ac7a3ee2295c55c2ef7efcaec3786be3d05
Anyeel <angelmillanaragon@hotmail.com>
2024-11-05 10:59:20 +0100

    clase 05/11 p5

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,16 +1,21 @@
+const vocales = "aeiou".split("")
+const consonantes = "qwrtypsdfghjklñzxcvbnm".split("")
+console.log(vocales, consonantes)
+
+function crearPalabra(n_silabas){
+    let palabra = ""
+    for(let i = 0; i < n_silabas; i++){
+      palabra += random(consonantes) + random(vocales)
+    }
+    return palabra
+  }
+
 function setup() {
     createCanvas(400, 400);
+    frameRate(1)
     background(220);
   }
   
   function draw() {
-    console.log(sin(frameCount/20)*100)
-    const velocidad = 100
-    const amplitud = 100
-    const posX = width/2
-    const posY = height/2
-    //ellipse(cos(frameCount/20)*100 + 200, frameCount, 1)
-    //ellipse(sin(frameCount/20)*100 + 200, frameCount, 1)
-    ellipse(noise(frameCount/velocidad)*amplitud + posX, frameCount, 1)
-    ellipse(frameCount, noise(frameCount/velocidad)*amplitud + posY, 1)
+    text(crearPalabra(random(5)), random(width), random(height));
   }

b7e1daeb644e19fd5ee44e3dc363ed8a471c1915
Anyeel <angelmillanaragon@hotmail.com>
2024-11-26 11:03:04 +0100

    asegurando mis keys

--- jsonbin-test/crud.py
+++ jsonbin-test/crud.py
@@ -1,8 +1,13 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-bin_id= "67458cbaad19ca34f8d08d79"
-XMasterKey = "$2a$10$pquz1xSlPVEwiQnQL9T5Eef9ak345Hq.XtAVYUDsqwsoSgB4rFiKO"
-XAccessKey = "$2a$10$OSyxSHOdS2ckxWW.Nr05RuOSYNnrTTv/an9NIDsjNkpINr4s8QKHK"
+load_dotenv()
+
+bin_id = os.getenv("BIN_ID")
+XMasterKey = os.getenv("X_MASTER_KEY")
+XAccessKey = os.getenv("X_ACCESS_KEY")
+
 url_root = "https://api.jsonbin.io/v3"
 route = f"/b/{bin_id}"
 headers = {

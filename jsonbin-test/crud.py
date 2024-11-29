be97c692fbd746b9ad4b939b792dba81690cb57f
Anyeel <angelmillanaragon@hotmail.com>
2024-11-29 11:23:07 +0100

    2 clase crud

--- jsonbin-test/crud.py
+++ jsonbin-test/crud.py
@@ -11,9 +11,26 @@ XAccessKey = os.getenv("X_ACCESS_KEY")
 url_root = "https://api.jsonbin.io/v3"
 route = f"/b/{bin_id}"
 headers = {
+    "Content-Type": "application/json",
     "X-Master-Key": XMasterKey,
     "X-Access-Key": XAccessKey
 }
+usuario = {
+    "user": "admin",
+    "password" : "1234"
+    }
+def read_db():
+    r = requests.get(url_root + route, headers=headers)
+    print(r.json())
 
-r = requests.get(url_root + route, headers=headers)
-print(r.json())
+def update_db():
+    r = requests.put(url_root + route, headers=headers, json=usuario)
+    print(r.json())
+
+def add_user():
+    r = requests.patch(url_root + route, headers=headers, json=usuario)
+    print(r.json())
+
+read_db()
+add_user()
+read_db()

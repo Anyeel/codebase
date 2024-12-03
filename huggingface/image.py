2117f8f7b969f65d9646d65c545be47809ebdef3
Anyeel <angelmillanaragon@hotmail.com>
2024-12-03 11:00:19 +0100

    images

---
+++ huggingface/image.py
@@ -0,0 +1,20 @@
+import requests
+import os
+import dotenv
+import io
+from PIL import Image
+dotenv.load_dotenv()
+
+KEY = os.getenv("KEY")
+API_URL = "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev"
+headers = {"Authorization": f"Bearer {KEY}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+image_bytes = query({
+	"inputs": "fight club 1999",
+})
+
+image = Image.open(io.BytesIO(image_bytes))
+image.save("fightclub.png")

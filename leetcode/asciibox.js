50d9a481fe161c95888a8518df5e9af93ac1d143
Anyeel <angelmillanaragon@hotmail.com>
2024-12-10 09:59:52 +0100

    correccionleetcode

--- leetcode/asciibox.js
+++ leetcode/asciibox.js
@@ -1,17 +1,17 @@
 let ancho = 3
-let largo = 3
+let alto = 3
 
 let Box = ["╔", "╗", "╚", "╝", "║", "═"]
 let fila = ""
-let columnas = "║"
+let columnas = Box[4]
 for (let i = 0; i < ancho; i++){
-    fila = fila + "═"
-    columnas = columnas + " "
+    fila += Box[5]
+    columnas += " "
 }
-columnas = columnas + "║"
+columnas += Box[4]
 
-console.log("╔" + fila + "╗")
-for (let i = 0; i < largo; i++){
+console.log(Box[0] + fila + Box[1])
+for (let i = 0; i < alto; i++){
     console.log(columnas)
 }
-console.log("╚" + fila + "╝")
+console.log(Box[2] + fila + Box[3])

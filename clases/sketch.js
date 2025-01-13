33b704408c0167c4763b4f819322a70a6a442a70
Anyeel <angelmillanaragon@hotmail.com>
2025-01-13 12:08:28 +0100

    fix sketch

--- clases/sketch.js
+++ clases/sketch.js
@@ -1,52 +1,56 @@
-const width = 400
-const length = 400
-const size = 100
-let particle
-let img 
+const width = 400;
+const height = 400;
+const size = 100;
+let particle;
+let img;
 
-function preload() { 
+function preload() {
     img = loadImage('berserk.png');
 }
+
 function setup() {
-    createCanvas(width, length)
-    particle = new Particle(width/2, length/2 , random(-3, 3), random(-3, 3))
+    createCanvas(width, height);
+    particle = new Particle(random(size / 2, width - size / 2), random(size / 2, height - size / 2), random(-3, 3), random(-3, 3));
 }
-  
+
 function draw() {
-    background(220)
-    particle.display()
-    particle.move()
+    background(220);
+    particle.display();
+    particle.move();
 }
 
+class Particle {
+    constructor(positionX, positionY, velocityX, velocityY) {
+        this.color = this.randomColor();
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+    }
 
-class Particle{
-    constructor(positionX, positionY, velocityX, velocityY){
-        this.color = this.randomColor()
-        this.positionX = positionX
-        this.positionY = positionY
-        this.velocityX = velocityX
-        this.velocityY = velocityY
-    }
-    detectBorder(){
-        if (this.positionX == width - size/2 || this.positionX == 0 + size/2){
-            this.velocityX *= -1
-            this.color = this.randomColor()
+    detectBorder() {
+        if (this.positionX >= width - size / 2 || this.positionX <= size / 2) {
+            this.velocityX *= -1;
+            this.color = this.randomColor();
         }
-        if (this.positionY == length - size/2 || this.positionY == 0 + size/2 ){
-            this.velocityY *= -1
-            this.color = this.randomColor()
+        if (this.positionY >= height - size / 2 || this.positionY <= size / 2) {
+            this.velocityY *= -1;
+            this.color = this.randomColor();
         }
     }
-    move(){
-        this.positionX += this.velocityX
-        this.positionY += this.velocityY
-        this.detectBorder()
+
+    move() {
+        this.positionX += this.velocityX;
+        this.positionY += this.velocityY;
+        this.detectBorder();
     }
-    randomColor(){
-        return color(random(255), random(255), random(255))
+
+    randomColor() {
+        return color(random(255), random(255), random(255));
     }
-    display(){
-        fill(this.color)
-        image(img, this.positionX - size/2, this.positionY - size/2, size, size)
+
+    display() {
+        fill(this.color);
+        image(img, this.positionX - size / 2, this.positionY - size / 2, size, size);
     }
 }

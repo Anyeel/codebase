3b866c74e8bf3bff6346dd749cfa7af7249abbd4
Anyeel <angelmillanaragon@hotmail.com>
2025-03-31 12:31:52 +0200

    logica frutas

--- frutas/server.js
+++ frutas/server.js
@@ -13,9 +13,9 @@ app.get("/frutas", (req, res) => {
 
 app.post("/frutas", (req, res) => {
     const fruta = req.body.fruta;
-    console.log("Esto ha llegado desde el cliente:", fruta)
-    frutas.push(fruta)
-    res.json(frutas)
+    console.log("Esto ha llegado desde el cliente:", fruta);
+    frutas.push(fruta);
+    res.json(frutas);
 })
 
-app.listen(3000, () => console.log("Servidor funcionando en 3000"))
+app.listen(3000, () => console.log("Servidor funcionando en el puerto 3000"))
